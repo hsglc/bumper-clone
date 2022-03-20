1b3c05dd155e448f3380cd552cf1bcf99e6fda77
huseyin gulcu <huseyingulcuu@gmail.com>
2022-03-20 22:43:55 +0300

    feat: Create the header's logo.

---
+++ components/HeaderLogo.js
@@ -0,0 +1,11 @@
+/** @format */
+
+const HeaderLogo = () => {
+  return (
+    <div className="z-50 font-black w-min text-xl py-[8px] px-[14px] bg-black text-white italic rounded-[20px]">
+      BUMPER
+    </div>
+  );
+};
+
+export default HeaderLogo;
