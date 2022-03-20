4e00bbe53672dc74de7e0e984446316c2b5a1355
huseyin gulcu <huseyingulcuu@gmail.com>
2022-03-20 22:44:26 +0300

    feat: Export all icons.

---
+++ icons/index.js
@@ -0,0 +1,19 @@
+/** @format */
+
+export { default as CompanyIcon } from "./company";
+export { default as CostIcon } from "./cost";
+export { default as EmailIcon } from "./email";
+export { default as HomeIcon } from "./home";
+export { default as NameIcon } from "./name";
+export { default as MarketIcon } from "./market";
+export { default as LeftArrowIcon } from "./leftArrow";
+export { default as PhoneNumberIcon } from "./phoneNumber";
+export { default as RightArrowIcon } from "./rightArrow";
+export { default as ServiceIcon } from "./service";
+export { default as SecureIcon } from "./secure";
+export { default as StarIcon } from "./star";
+export { default as SuccessCheckIcon } from "./successCheck";
+export { default as QuotationMarkIcon } from "./quotationMark";
+export { default as PlusIcon } from "./plus";
+export { default as LoginIcon } from "./login";
+
