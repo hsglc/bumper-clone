ddb05b643bfec6837c63e74468b50887896f401f
huseyin gulcu <huseyingulcuu@gmail.com>
2022-03-20 22:43:27 +0300

    feat: Create a custom button.

---
+++ components/Button.js
@@ -0,0 +1,28 @@
+/** @format */
+
+import { useRouter } from "next/router";
+
+import { RightArrowIcon } from "../icons";
+
+const Button = ({
+  children,
+  icon = <RightArrowIcon />,
+  route = "",
+  type = "",
+}) => {
+  const { replace } = useRouter();
+
+  return (
+    <button
+      type={type}
+      onClick={() => route && replace(route)}
+      className="group w-full flex items-center justify-center gap-2.5 h-[45px] py-2.5 border border-[#1B1B1B] rounded-[27px] bg-[#32BE50] active:bg-[#1B1B1B]">
+      <span className="text-black transition-all group-hover:text-white group-active:text-white">
+        {children}
+      </span>
+      <i> {icon}</i>
+    </button>
+  );
+};
+
+export default Button;
