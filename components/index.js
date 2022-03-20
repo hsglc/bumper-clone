1ce0631f3dc74e11cb28b1765a13f87025876bf3
huseyin gulcu <huseyingulcuu@gmail.com>
2022-03-20 22:44:08 +0300

    feat: Export all components.

---
+++ components/index.js
@@ -0,0 +1,8 @@
+/** @format */
+
+export { default as TextWithIcon } from "./Text.js";
+export { default as Input } from "./Input.js";
+export { default as Button } from "./Button.js";
+export { default as HeaderLogo } from "./HeaderLogo.js";
+export { default as Header } from "./Header.js";
+export { default as MultiSelectButton } from "./MultiSelectButton";
