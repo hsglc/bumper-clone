c3e629d866df3813207ac56edbec415f2ca97e91
huseyin gulcu <huseyingulcuu@gmail.com>
2022-03-23 22:47:29 +0300

    ffeat: Export registerfform.

--- components/index.js
+++ components/index.js
@@ -6,3 +6,4 @@ export { default as Button } from "./Button.js";
 export { default as HeaderLogo } from "./HeaderLogo.js";
 export { default as Header } from "./Header.js";
 export { default as MultiSelectButton } from "./MultiSelectButton";
+export { default as RegisterForm } from "./RegisterForm";
