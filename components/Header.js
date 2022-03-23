385c4d32a66fa6342c5e9978a0d14f0ab3fa1adc
huseyin gulcu <huseyingulcuu@gmail.com>
2022-03-23 22:47:10 +0300

    refactor: Add router and seperate mobile and lgScreen layout.

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useMediaQuery } from "react-responsive";
 
 import { HeaderLogo, TextWithIcon } from "../components";
@@ -9,9 +10,11 @@ import { LoginIcon, ServiceIcon } from "../icons";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+
   const isLgScreen = useMediaQuery({
     query: "(min-width: 1024px)",
   });
+  const router = useRouter();
 
   const leftSideLinks = (
     <div className={` bg-[#FF733C] `}>
@@ -23,72 +26,85 @@ const Header = () => {
           </Link>
         </li>
         <li className="navbar-leftside-elements">
-          <Link href="/works">
+          <Link href="/">
             <a className="pl-8">How it works</a>
           </Link>
         </li>
         <li className="navbar-leftside-elements">
-          <Link href="/about">
+          <Link href="/">
             <a className="pl-8">About Us</a>
           </Link>
         </li>
         <li className="navbar-leftside-elements">
-          <Link href="/blog">
+          <Link href="/">
             <a className="pl-8">Blog</a>
           </Link>
         </li>
       </ul>
     </div>
   );
-  console.log(isOpen);
+  const mobileScreenLayout = (
+    <>
+      <HeaderLogo />
+      <nav>
+        <div
+          onClick={() => setIsOpen((prevVal) => !prevVal)}
+          className="navbar-icon">
+          <div
+            className={`navbar-icon-slices  ${
+              isOpen ? "-rotate-45  absolute" : "rotate-0"
+            }`}
+          />
+          <div
+            className={`navbar-icon-slices ${
+              isOpen ? "opacity-0" : "opacity-100"
+            } `}
+          />
+
+          <div
+            className={`navbar-icon-slices ${
+              isOpen ? "rotate-45  absolute" : "rotate-0"
+            }`}
+          />
+        </div>
+      </nav>
+    </>
+  );
+
+  const lgScreenLayout = (
+    <>
+      {leftSideLinks}
+      <HeaderLogo />
+      <div className="flex items-center gap-3">
+        <div
+          className="relative cursor-pointer"
+          onClick={() => router.replace("/register")}>
+          <TextWithIcon
+            text="Partner sign up"
+            order="fromRight"
+            icon={<ServiceIcon fill="black" />}
+          />
+          {router?.pathname === "/register" && (
+            <div
+              data-testid="register"
+              className="bg-black w-32 rounded-tl-lg rounded-tr-lg h-2 absolute -bottom-4"></div>
+          )}
+        </div>
+        <div data-testid="register-btn" className="py-1.5 px-2.5 border border-black rounded-full cursor-pointer transition-all hover:bg-white">
+          <TextWithIcon
+            text="Partner Login"
+            order="fromRight"
+            icon={<LoginIcon />}
+          />
+        </div>
+      </div>
+    </>
+  );
 
   return (
     <div className="lg:bg-[#5A698C] lg:p-6">
       <header className="layout-container py-5 lg:py-1.5 bg-[#FF733C] flex justify-between items-center lg:rounded-r-full lg:rounded-l-full">
-        {isLgScreen && leftSideLinks}
-        <HeaderLogo />
-
-        {!isLgScreen && (
-          <nav>
-            <div
-              onClick={() => setIsOpen((prevVal) => !prevVal)}
-              className="navbar-icon">
-              <div
-                className={`navbar-icon-slices  ${
-                  isOpen ? "-rotate-45  absolute" : "rotate-0"
-                }`}
-              />
-              <div
-                className={`navbar-icon-slices ${
-                  isOpen ? "opacity-0" : "opacity-100"
-                } `}
-              />
-
-              <div
-                className={`navbar-icon-slices ${
-                  isOpen ? "rotate-45  absolute" : "rotate-0"
-                }`}
-              />
-            </div>
-          </nav>
-        )}
-
-        {isLgScreen && (
-          <div className="flex items-center gap-3">
-            <TextWithIcon
-              text="Partner sign up"
-              order="fromRight"
-              icon={<ServiceIcon fill="black" />}
-            />
-            <div className="py-1.5 px-2.5 border border-black rounded-full cursor-pointer transition-all hover:bg-white">
-              <TextWithIcon
-                text="Partner Login"
-                order="fromRight"
-                icon={<LoginIcon />}
-              />
-            </div>
-          </div>
-        )}
+        {isLgScreen ? lgScreenLayout : mobileScreenLayout}
       </header>
       {isOpen && !isLgScreen && leftSideLinks}
     </div>
