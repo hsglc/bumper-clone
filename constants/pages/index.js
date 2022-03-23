ce889c170428770b61486b63e61cca5235331894
huseyin gulcu <huseyingulcuu@gmail.com>
2022-03-23 22:49:39 +0300

    feat: Create constant file for pages.

---
+++ constants/pages/index.js
@@ -0,0 +1,69 @@
+import { SecureIcon, CostIcon, MarketIcon } from "../../icons";
+
+export const payNowSections = [
+  {
+    id: 1,
+    icon: <SecureIcon />,
+    title: "Secure",
+    desc: "Prevent customers from revealing sensitive information over the phone, GDPR safe and tested.",
+  },
+  {
+    id: 2,
+    icon: <CostIcon />,
+    title: "Low cost",
+    desc: "Low transaction fee, no setup or mobile phone fees.",
+  },
+  {
+    id: 3,
+    icon: <MarketIcon />,
+    title: "Fast",
+    desc: "Set up and ready to go in minutes!",
+  },
+];
+
+export const asSimpleAsSections = [
+  {
+    id: 1,
+    title: "Fix it",
+    desc: "Your customers bring their vehicle to you. You repair and service the car. Everything just like it works right now.",
+  },
+  {
+    id: 2,
+    title: "Split it",
+    desc: "When the customer gets their bill or quote, Bumper either intergrates into your existing online checkout or can be done on-site before you hand back the keys. All in just a few clicks.",
+  },
+  {
+    id: 3,
+    title: "Sorted",
+    desc: "You and your customer part ways happy. You’re paid in full direct from Bumper, the customer repays Bumper over their chosen payment plan.",
+  },
+];
+
+export const benefitsSections = [
+  {
+    id: 1,
+    percent: "71",
+    content:
+      "of customers authorised more work because they used Bumper to split their bill.",
+  },
+  {
+    id: 2,
+    percent: "90",
+    content:
+      "of customers would return to the same garage because they offered Bumper",
+  },
+  {
+    id: 3,
+    percent: "+350",
+    content:
+      "increase in average invoice value of customers using Bumper to split their bill.",
+  },
+  {
+    id: 4,
+    percent: "89",
+    content:
+      "of customers felt less stressed because they used bumper to spread the cost.",
+  },
+];
+
+
